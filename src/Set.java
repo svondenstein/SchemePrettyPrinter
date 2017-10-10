b08eba769937931b0b97a0982834986d0bb1b43b
Stephen Vondenstein <shvondenstein@gmail.com>
2017-10-09 22:07:30 -0500

    Fixed indentation error

--- src/Set.java
+++ src/Set.java
@@ -1,15 +1,9 @@
-import java.io.*;
-
 class Set extends Special {
     public Set(){}
 
     void print(Node t, int n, boolean p) {
         if(!p) {
-            for(int i = 0; i < n; i++) {
-                System.out.print(" ");
-            }
             System.out.print("(");
-            //p = true;
         }
         t.getCar().print(n, false);
         if(!t.getCdr().isNull())
